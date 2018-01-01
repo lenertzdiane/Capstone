e6b3da32c0fff2acafd589b04fda7d1f67f005e6
Diane Lenertz <lenertzd@grinnell.edu>
2017-12-31 22:47:27 -0800

    add marker to end of stroke-dashoffset animated line, remove points stylistically

--- leaflet/leaflet-setup.js
+++ leaflet/leaflet-setup.js
@@ -1,10 +1,12 @@
 //SATURDAY:
-// code to scale animation for zooming // i think i just have to be able to reset lengths array and projectedArray before rendering just can't figure out where to do it
-// currently it resets itself to 0 have to figure out a way reset it at a transformation of the previous one
-// wrap render logic in functions
+// ======================================================= code to scale animation for zooming // i think i just have to be able to reset lengths array and projectedArray before rendering just can't figure out where to do it
+// ======================================================= currently it resets itself to 0 have to figure out a way reset it at a transformation of the previous one
+// ======================================================= wrap render logic in functions
 // attach clickable markers to the map surrounding that have popups come up
+
 // go through and annotate this code
 // marker that also moves?
+
 // NEED TO REORGANIZE THIS CODE
 
 //Phase 2:
@@ -71,266 +73,236 @@ $(document).ready(function() {
     var projectedArray = []
 
     var makeLine = d3.svg.line()
-    .x(function(d) {
-      return applyLatLngToLayer(d).x})
-      .y(function(d) {return applyLatLngToLayer(d).y})
-      .interpolate("linear");
-
-      var linePath = g.selectAll(".lines")
-      .data([geoData])
-      .enter()
-      .append("path")
-      .attr("class", "lines")
-
-
-      var ptFeatures = g.selectAll("circle")
-      .data(geoData)
-      .enter()
-      .append("circle")
-      .attr("r", 3)
-
-
-      // .attr("class", function(d) {
-      //   return "waypoints " + "c" + d.properties.time
-      // });
-
-
-      // var marker = g.append("circle")
-      // .attr("r", 10)
-      // .attr("id", "marker")
-      // .attr("class", "travelMarker");
-
-      reset();
-      map.on("viewreset", reset);
-      map.on("moveend", reset);
-
-
-
-      var scrollTop = 0
-      var newScrollTop = 0
-
-      function reset() {
-
-        container = d3.select('#container')
-
-        container.on ("scroll", function() {
-          newScrollTop = container.node().scrollTop;
-        });
-
-
-        // console.log('in reset');
-        var bounds = path.bounds(incidents),
-        topLeft = bounds[0],
-        bottomRight = bounds[1]
-
-        map.on("viewreset", change);
-        map.on("moveend", change);
-
-        function change() {
-          linePath
-          .style('stroke-dashoffset', function(d) {
-            return length - 0 + 'px';
-          })
-          .style('stroke-dasharray', length)
-          //THIS IS A DIRTY HACK!
-          scrollTop = scrollTop +1
-        }
-
-
-        // console.log(topLeft);
-        // console.log(bottomRight);
-
-        ptFeatures.attr("transform",
-        function(d) {
-          return "translate(" +
-          applyLatLngToLayer(d).x + "," +
-          applyLatLngToLayer(d).y + ")";
-        });
-        // again, not best practice, but I'm harding coding
-        // the starting point
-
-
-
-        // marker.attr("transform",
-        // function() {
-        //   var y = geoData[0].geometry.coordinates[1]
-        //   var x = geoData[0].geometry.coordinates[0]
-        //   return "translate(" +
-        //   map.latLngToLayerPoint(new L.LatLng(y, x)).x + "," +
-        //   map.latLngToLayerPoint(new L.LatLng(y, x)).y + ")";
-        // });
-        // Setting the size and location of the overall SVG container
-
-        svg.attr("width", bottomRight[0] - topLeft[0] + 120)
-        .attr("height", bottomRight[1] - topLeft[1] + 120)
-        .style("left", topLeft[0] - 50 + "px")
-        .style("top", topLeft[1] - 50 + "px");
-        // linePath.attr("d", d3path);
-
-        linePath.attr("d", makeLine)
-        // ptPath.attr("d", d3path);
-        g.attr("transform", "translate(" + (-topLeft[0] + 50) + "," + (-topLeft[1] + 50) + ")");
-
-
-        let distance = 0
-        let lengthsArray =[]
-
-        projectedArray = projectedArray.slice((projectedArray.length - 7), (projectedArray.length))
-
-
-
-        var getDistance = function getDistance(point1, point2) {
-          var xs = 0;
-          var ys = 0;
-
-          xs = point2[0] - point1[0];
-          xs = xs * xs;
-
-          ys = point2[1] - point1[1];
-          ys = ys * ys;
-
-          return Math.sqrt( xs + ys );
-        }
-        // console.log(projectedArray.length);
-        // console.log(projectedArray);
-
-        for (i = 0; i < projectedArray.length-1; i++) {
-          distance = getDistance(projectedArray[i], projectedArray[i+1]);
-          lengthsArray.push(distance);
-        }
-
-        ///////////// ANIMATIONS
-        let length = linePath.node().getTotalLength()
-
-
-
-
-
-        function makeTestPosition(scrollTop, number) {
-          if(number === 0) {
-            return 0
-          }
-          else {
-            return $('#'+number).position().top + scrollTop - ($(window).innerHeight())
-          }
-        }
-
-        function makeLastTestPosition(scrollTop, number) {
-          if(number === 0){
-            return 0
-          } else {
-            return $('.last' + number).position().top + scrollTop - ($(window).innerHeight())
-          }
-        }
-
-        function makeSegLength(lengthsArray, number) {
-          let total = 0
-          if(number === 0) {
-            return 0
-          }
-          else {
-            for (let i = 0; i < number; i ++){
-              total = total + lengthsArray[i]
-            }
-            console.log(lengthsArray);
-            return total
-          }
-        }
-
-
-        function makeLinePathScale(scrollTop, number){
-          var linePathScale = d3.scale.linear()
-          .domain([makeLastTestPosition(scrollTop, number-1), makeTestPosition(scrollTop, number)])
-          .range([makeSegLength(lengthsArray, number-1), makeSegLength(lengthsArray, number)])
-          .clamp(true);
-          return linePathScale(scrollTop)
-        }
-
-
-
-
-        var render = function() {
-          if (scrollTop !== newScrollTop) {
-            scrollTop = newScrollTop
-            if($('.last1').position().top > $(window).innerHeight()) {
-              console.log('in one');
-              linePath
-              .style('stroke-dashoffset', function(d) {
-                return length - makeLinePathScale(scrollTop, 1) + 'px';
-              })
-              .style('stroke-dasharray', length)
-            }
-
-            else if($('.last2').position().top > $(window).innerHeight()) {
-              console.log('in two');
-              linePath
-              .style('stroke-dashoffset', function(d) {
-                return length - makeLinePathScale(scrollTop, 2) + 'px';
-              })
-            }
-
-            else if($('.last3').position().top > $(window).innerHeight()){
-              console.log('in threeeeee');
-              linePath
-              .style('stroke-dashoffset', function(d) {
-                return length - makeLinePathScale(scrollTop, 3) + 'px';
-              })
-            }
-
-            else if($('.last4').position().top > $(window).innerHeight()){
-              console.log('in four');
-              linePath
-              .style('stroke-dashoffset', function(d) {
-                return length - makeLinePathScale(scrollTop, 4) + 'px';
-              })
-            }
-
-            else if($('.last5').position().top > $(window).innerHeight()){
-              console.log('in five');
-              linePath
-              .style('stroke-dashoffset', function(d) {
-                return length - makeLinePathScale(scrollTop, 5) + 'px';
-              })
-            }
-
-            else if($('.last6').position().top > $(window).innerHeight()){
-              console.log('in six');
-              linePath
-              .style('stroke-dashoffset', function(d) {
-                return length - makeLinePathScale(scrollTop, 6) + 'px';
-              })
-            }
-
-          }
-          window.requestAnimationFrame(reset)
-        }
-
-        window.requestAnimationFrame(render)
-
-      } // end reset
-
-
-      // CHANGING LATLONG TO LEAFLET LATLONG //////////////////////////////////////////////////// //////////////////////////////////////
-
-      function projectPoint(x, y) {
-
-        //how does this function know to pass lat and long from json data?
-        //latLngtoLayerPoint: Given a geographical coordinate, returns the corresponding pixel coordinate relative to the origin pixel.
-        //transforms geographical data into data relative to map
-        var point = map.latLngToLayerPoint(new L.LatLng(y, x));
-        // point is ex: {x:621 , y:898}
-        // returns output stream for the specified output stream...maybe point.x, point.y is specified output stream?
-        projectedArray.push([point.x, point.y])
-        this.stream.point(point.x, point.y);
-      }
-
-      function applyLatLngToLayer(d) {
-        var y = d.geometry.coordinates[1]
-        var x = d.geometry.coordinates[0]
-        return map.latLngToLayerPoint(new L.LatLng(y, x))
-      }
-
-
+    .x(function(d) {return applyLatLngToLayer(d).x})
+    .y(function(d) {return applyLatLngToLayer(d).y})
+    .interpolate("linear");
+
+    var linePath = g.selectAll(".lines")
+    .data([geoData])
+    .enter()
+    .append("path")
+    .attr("class", "lines")
+
+
+    var ptFeatures = g.selectAll("circle")
+    .data(geoData)
+    .enter()
+    .append("circle")
+    .attr("r", 0)
+
+
+    var marker = g.append("circle")
+    .attr("r", 10)
+    .attr("id", "marker")
+    .attr("class", "travelMarker");
+
+    reset();
+    map.on("viewreset", reset);
+    map.on("moveend", reset);
+
+    marker.attr("transform",
+    function() {
+      var y = geoData[0].geometry.coordinates[1]
+      var x = geoData[0].geometry.coordinates[0]
+      return "translate(" +
+      map.latLngToLayerPoint(new L.LatLng(y, x)).x + "," +
+      map.latLngToLayerPoint(new L.LatLng(y, x)).y + ")";
     });
 
+    var scrollTop = 0
+    var newScrollTop = 0
+
+    scrollTop = scrollTop + 1
+
+    function reset() {
+
+      container = d3.select('#container')
+
+      container.on ("scroll", function() {
+        newScrollTop = container.node().scrollTop;
+      });
+
+      var bounds = path.bounds(incidents),
+      topLeft = bounds[0],
+      bottomRight = bounds[1]
+
+      map.on("viewreset", change);
+      map.on("moveend", change);
+
+      function change() {
+        linePath
+        .style('stroke-dashoffset', function(d) {
+          return length - 0 + 'px';
+        })
+        .style('stroke-dasharray', length)
+        //THIS IS A DIRTY HACK!
+        scrollTop = scrollTop +1
+      }
+
+      ptFeatures.attr("transform",
+      function(d) {
+        return "translate(" +
+        applyLatLngToLayer(d).x + "," +
+        applyLatLngToLayer(d).y + ")";
+      });
+      // again, not best practice, but I'm harding coding
+      // the starting point
+
+
+      // Setting the size and location of the overall SVG container
+
+      svg.attr("width", bottomRight[0] - topLeft[0] + 120)
+      .attr("height", bottomRight[1] - topLeft[1] + 120)
+      .style("left", topLeft[0] - 50 + "px")
+      .style("top", topLeft[1] - 50 + "px");
+      // linePath.attr("d", d3path);
+
+      linePath.attr("d", makeLine)
+      // ptPath.attr("d", d3path);
+      g.attr("transform", "translate(" + (-topLeft[0] + 50) + "," + (-topLeft[1] + 50) + ")");
+
+      let distance = 0
+      let lengthsArray =[]
+
+      // This is also kind of a dirty hack I should be able to reset projected array somewhere but.
+      projectedArray = projectedArray.slice((projectedArray.length - geoData.length), (projectedArray.length))
+
+      var getDistance = function getDistance(point1, point2) {
+        var xs = 0;
+        var ys = 0;
+
+        xs = point2[0] - point1[0];
+        xs = xs * xs;
+
+        ys = point2[1] - point1[1];
+        ys = ys * ys;
+
+        return Math.sqrt( xs + ys );
+      }
+
+      for (i = 0; i < projectedArray.length-1; i++) {
+        distance = getDistance(projectedArray[i], projectedArray[i+1]);
+        lengthsArray.push(distance);
+      }
+
+      ///////////// ANIMATIONS
+      let length = linePath.node().getTotalLength()
+
+
+      function makeTestPosition(scrollTop, number) {
+        if(number === 0) {
+          return 0
+        }
+        else {
+          return $('#'+number).position().top + scrollTop - ($(window).innerHeight())
+        }
+      }
+
+      function makeLastTestPosition(scrollTop, number) {
+        if(number === 0){
+          return 0
+        } else {
+
+          return $('#' + number + '.last').position().top + scrollTop - ($(window).innerHeight())
+        }
+      }
+
+      function makeSegLength(lengthsArray, number) {
+        let total = 0
+        if(number === 0) {
+          return 0
+        }
+        else {
+          for (let i = 0; i < number; i ++){
+            total = total + lengthsArray[i]
+          }
+          return total
+        }
+      }
+
+
+      function makeLinePathScale(scrollTop, number){
+        var linePathScale = d3.scale.linear()
+        .domain([makeLastTestPosition(scrollTop, number-1), makeTestPosition(scrollTop, number)])
+        .range([makeSegLength(lengthsArray, number-1), makeSegLength(lengthsArray, number)])
+        .clamp(true);
+        return linePathScale(scrollTop)
+      }
+
+      var render = function() {
+        if (scrollTop !== newScrollTop) {
+          scrollTop = newScrollTop
+
+          // select all spans with class last
+
+          let elements = $("span[class='last']");
+
+          // let elements =[$('.last1'), $('.last2'), $('.last3'), $('.last4'), $('.last5')]
+
+          for(i = 0; i < elements.length; i ++) {
+            if($(elements[i]).position().top > $(window).innerHeight()){
+              actingElement = $(elements[i])
+              break
+            }
+          }
+
+
+
+          linePath
+          .style('stroke-dashoffset', function(d) {
+            num = parseInt(actingElement[0].id)
+
+            return length - makeLinePathScale(scrollTop, actingElement[0].id) + 'px';
+          })
+          .style('stroke-dasharray', length)
+          .style('stroke-width', function() {
+            if(map.getZoom() > 16) {
+              return 9
+            } else if(14 < map.getZoom < 16) {
+              return 5
+            } else {
+              return 2
+            }
+          })
+          .style('stroke-dasharray', length)
+
+          console.log(linePath.style('stroke-dashoffset'));
+          // console.log($('path')[0].attributes.style);
+
+          var p = linePath.node().getPointAtLength(length - parseInt(linePath.style('stroke-dashoffset')));
+          console.log(p);
+          marker.attr("transform", "translate(" + p.x + "," + p.y + ")");
+
+        }
+        window.requestAnimationFrame(reset)
+      }
+
+      window.requestAnimationFrame(render)
+
+    } // end reset
+
+
+    // CHANGING LATLONG TO LEAFLET LATLONG //////////////////////////////////////////////////// //////////////////////////////////////
+
+    function projectPoint(x, y) {
+
+      //how does this function know to pass lat and long from json data?
+      //latLngtoLayerPoint: Given a geographical coordinate, returns the corresponding pixel coordinate relative to the origin pixel.
+      //transforms geographical data into data relative to map
+      var point = map.latLngToLayerPoint(new L.LatLng(y, x));
+      // point is ex: {x:621 , y:898}
+      // returns output stream for the specified output stream...maybe point.x, point.y is specified output stream?
+      projectedArray.push([point.x, point.y])
+      this.stream.point(point.x, point.y);
+    }
+
+    function applyLatLngToLayer(d) {
+      var y = d.geometry.coordinates[1]
+      var x = d.geometry.coordinates[0]
+      return map.latLngToLayerPoint(new L.LatLng(y, x))
+    }
+
+
   });
+
+});
